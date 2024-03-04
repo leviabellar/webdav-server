29df76ca2bfb657ef0df0cbfe6e49392711ff059
Levi Donio Abellar <levi.abellar@lexmark.com>
2024-03-04 15:10:10 +0800

    with express

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const webdav = require('webdav-server').v2;
+const express = require('express');
  
-const server = new webdav.WebDAVServer({
-    port: 1900
-});
+const server = new webdav.WebDAVServer();
+const app = express();
  
-server.afterRequest((arg, next) => {
-    // Display the method, the URI, the returned status code and the returned message
-    console.log('>>', arg.request.method, arg.requested.uri, '>', arg.response.statusCode, arg.response.statusMessage);
-    // If available, display the body of the response
-    console.log(arg.responseBody);
-    next();
-});
- 
-server.start(() => console.log('READY'));
+// Mount the WebDAVServer instance
+app.use(webdav.extensions.express('/my/sub/path', server));
+app.listen(8080); // Start the Express server
